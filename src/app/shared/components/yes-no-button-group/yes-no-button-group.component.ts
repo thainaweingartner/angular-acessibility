75a09fd8e702514efe99236584a42c935c0b03cc
Thainá Weingartner Chagas <thaina.chagas@pti.org.br>
2022-01-28 17:11:26 -0300

    Modulo 04

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 
 @Component({
   selector: 'app-yes-no-button-group',
@@ -18,11 +19,14 @@ export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   @Input() public value: string = null;
   @Input() public label = '';
   @Output() public valueChanged = new EventEmitter<string>();
+  public id: string = null;
   public options = YesNoButtonGroupOptions;
   public onChange = (value: string) => {};
   public onTouched = () => {};
 
-  constructor() { }
+  constructor(uniqueIdService: UniqueIdService) {
+    this.id = uniqueIdService.generateUniqueIdWithPrefix('yes-no-button-group');
+  }
 
   ngOnInit(): void {
   }
